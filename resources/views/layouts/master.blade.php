5d59e97c1c67891991cf9a2b7978ef3af7e64653
Charles Henderson <39602704+agingdanger@users.noreply.github.com>
2020-03-15 02:20:41 -0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,8 @@
 
 <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.css') }}" />
 
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
 <!-- This is for the jQuery Datatable-->
 <!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
   
